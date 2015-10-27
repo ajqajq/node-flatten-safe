319ae968aecaa910557f6737caafa86bd1f4e674
Dave Beck <ajqajq@gmail.com>
2015-10-27 12:16:15 -0500

    Modifies the flattener to simply remove all duplicates and move everything up to the root node_modules with name and version for directory.  Everything works.  Need to test if this messes up node.

--- flatten.js
+++ flatten.js
@@ -1,11 +1,53 @@
 'use strict';
-var Flattener = require ('./lib/folderFlattener.js');
-var Tree = require ('./lib/bftTopDownFlatteningTree.js');
+
+var Acorn = require('./lib/Acorn.js');
+var fs = require('fs');
+var rimraf = require('rimraf');
 var FOLDER_ARG_OFFSET = 2;
 var DEFAULT_FOLDER_ARG_OFFSET = 1;
 var folderToFlatten = process.argv[FOLDER_ARG_OFFSET];
 if (typeof folderToFlatten == 'undefined') {
     folderToFlatten = process.argv[DEFAULT_FOLDER_ARG_OFFSET];
 }
-var flattener = new Flattener(Tree);
-flattener.flatten(folderToFlatten);
+var acorn = new Acorn(folderToFlatten);
+acorn.grow().then((tree) => {
+    var allModules = {};
+
+    //Note: should this be moved into the Tree class as a prune function?
+    //Remove all duplicates and copy all modules into the root node_modules folder.
+    tree.dftVisit((node) => {
+        if (node == tree.root) return; // Don't try to move the root.
+        //allModules is a dictionary of versions where name is the key. i.e. Dictionary<name,List<version>>
+        if (allModules[node.value.name] == undefined) {
+            allModules[node.value.name] = [node.value.version]; // This is the first instance of this module
+            moveModuleToPath(getPath(folderToFlatten, node.value), node.value);
+        } else if (contains(allModules[node.value.name], node.value.version)) {
+            node.parent.removeChild(node); // Remove ourselves and don't copy, we're just a clone!
+            node.children = new Array(); // Jettison all of my children.  This makes sure we don't traverse them.
+            console.log("Removing duplicate - " + node.value.name + " version " + node.value.version);
+            rimraf(node.value.path, (err) => {
+                if (err) console.log(err);
+            });
+        } else {
+            allModules[node.value.name].push(node.value.version);
+            moveModuleToPath(getPath(folderToFlatten, node.value), node.value);
+        }
+    });
+
+    function contains(names, name) {
+        var index = names.indexOf(name);
+        return index != -1;
+    }
+
+    function moveModuleToPath(path, moduleInfo) {
+        console.log("Moving " + moduleInfo.path + " to " + path);
+        fs.rename(moduleInfo.path, path, (err) => {
+            if (err) console.log(err)
+        });
+
+    }
+
+    function getPath(folderPath, moduleInfo) {
+        return folderPath + "\\node_modules\\" + moduleInfo.name + "_" + moduleInfo.version;
+    }
+});
--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 var Tree = require('./lib/tree/Tree.js');
 var Node = require('./lib/tree/TreeNode.js');
+var Acorn = require('./lib/Acorn.js');
 
 function TreeWithNoConflictsShouldCompletelyFlatten() {
 
@@ -10,25 +11,23 @@ function TreeWithNoConflictsShouldCompletelyFlatten() {
     var b = new Node('b');
     var c = new Node('c');
     var d = new Node('d');
-    root.addChild (a);
-    root.addChild (b);
-    b.addChild (c);
-    c.addChild (d);
-    var tree = new Tree (root);
-    console.log ('\nRunning test TreeWithNoConflictsShouldCompletelyFlatten');
+    root.addChild(a);
+    root.addChild(b);
+    b.addChild(c);
+    c.addChild(d);
+    var tree = new Tree(root);
+    console.log('\nRunning test TreeWithNoConflictsShouldCompletelyFlatten');
     tree.flatten();
-    
+
     //Asserts:
-    if (a.children.length != 0) console.log ('Test Failed - a has children.');
-    if (b.children.length != 0) console.log ('Test Failed - b has children.');
-    if (c.children.length != 0) console.log ('Test Failed - c has children.');
-    if (d.children.length != 0) console.log ('Test Failed - d has children.');
-    if (root.children.length != 4) console.log ('Test Failed - root does not have 4 children.');
-    
-    console.log ('Test Complete');
-    
-    console.log ('Printout of tree=\n');
+    if (a.children.length != 0) console.log('Test Failed - a has children.');
+    if (b.children.length != 0) console.log('Test Failed - b has children.');
+    if (c.children.length != 0) console.log('Test Failed - c has children.');
+    if (d.children.length != 0) console.log('Test Failed - d has children.');
+    if (root.children.length != 4) console.log('Test Failed - root does not have 4 children.');
+
     tree.print();
+    console.log('***Test Complete***\n');
 }
 
 function TreeWithConflictsShouldFlattenCorrectly() {
@@ -38,28 +37,40 @@ function TreeWithConflictsShouldFlattenCorrectly() {
     var b = new Node('b');
     var c = new Node('c');
     var d = new Node('d');
-    var d2 = new Node ('d');
-    root.addChild (a);
-    root.addChild (b);
-    root.addChild (d2);
-    b.addChild (c);
-    c.addChild (d);
-    var tree = new Tree (root);
-    console.log ('\nRunning test TreeWithConflictsShouldFlattenCorrectly');
+    var d2 = new Node('d');
+    root.addChild(a);
+    root.addChild(b);
+    root.addChild(d2);
+    b.addChild(c);
+    c.addChild(d);
+    var tree = new Tree(root);
+    console.log('\nRunning test TreeWithConflictsShouldFlattenCorrectly');
     tree.flatten();
-    
+
     //Asserts:
-    if (a.children.length != 0) console.log ('Test Failed - a has children.');
-    if (b.children.length != 0) console.log ('Test Failed - b has children.');
-    if (c.children.length != 1) console.log ('Test Failed - c should have one child (d).');
-    if (d.children.length != 0) console.log ('Test Failed - d has children.');
-    if (root.children.length != 4) console.log ('Test Failed - root does not have 4 children.');
-    
-    console.log ('Test Complete');
-    
-    console.log ('Printout of tree=\n');
+    if (a.children.length != 0) console.log('Test Failed - a has children.');
+    if (b.children.length != 0) console.log('Test Failed - b has children.');
+    if (c.children.length != 1) console.log('Test Failed - c should have one child (d).');
+    if (d.children.length != 0) console.log('Test Failed - d has children.');
+    if (root.children.length != 4) console.log('Test Failed - root does not have 4 children.');
     tree.print();
+    console.log('***Test Complete***\n');
+}
+
+function AcornShouldGrowTreeFromDirectoryAndFlattenCorrectly() {
+    console.log('\nRunning test AcornShouldGrowTreeFromDirectoryAndFlattenCorrectly');
+    var acorn = new Acorn('C:/testData/flattenTest');
+    acorn.grow().then((tree) => {
+        console.log('tree (unflattened) = ' + tree);
+        tree.print();
+        tree.flatten();
+        console.log('tree (flattened) = ' + tree);
+        tree.print();
+        if (tree.root.children.length != 4) console.log("Test Failed - root does not have 4 children.");
+        console.log('***Test Complete***\n');
+    });
 }
 
 TreeWithNoConflictsShouldCompletelyFlatten();
 TreeWithConflictsShouldFlattenCorrectly();
+AcornShouldGrowTreeFromDirectoryAndFlattenCorrectly();
